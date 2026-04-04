e489d760c33325aa6afaaf8c8567111a7923cae5
Danyal <danyalyousuf05@gmail.com>
2026-04-03 22:03:13 -0400

    Created branch

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,48 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route, Link, useLocation} from "react-router-dom";
+import Login from './Login.jsx';
+
+
+function Home() {
+  const location = useLocation();
+  const successMsg = location.state?.message;
+  return (
+    <>
+      <div>
+        {successMsg && (
+          <div className="success">
+            {successMsg}
+          </div>
+        )}
+      </div>
+      
+      <p>SFWRENG 3A04 Deliverable 4</p>
+        <ul>
+          <li>Angad Chhabra</li>
+          <li>Jerry Jing</li>
+          <li>Danyal Yousuf</li>
+          <li>Kristian Diana</li>
+        </ul>
+        <Link to="/login">
+          <button>Log In</button>
+        </Link>
+        
+        <button>Sign Up</button>
+    </>
+  );
+}
+
 function App() {
   return (
-    <main>
-      <p>SFWRENG 3A04 Deliverable 4</p>
-      <ul>
-        <li>Angad Chhabra</li>
-        <li>Jerry Jing</li>
-        <li>Danyal Yousuf</li>
-        <li>Kristian Diana</li>
-      </ul>
-    </main>
+    <Router>
+      <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+      </main>
+    </Router>
   );
 }
 
